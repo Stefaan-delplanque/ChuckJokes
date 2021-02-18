edaf87424666d51af6b570887deea2dce1102f19
r0785168@student.thomasmore.be <Md8118776>
2021-02-18 18:10:47 +0100

    Our first PWA.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chuckJokes';
+  joke;
+
+  constructor(update: SwUpdate, private data: DataService) {
+    update.available.subscribe(() => {
+      update.activateUpdate().then(() => document.location.reload());
+    });
+  }
+
+  // tslint:disable-next-line:typedef use-lifecycle-interface
+  ngOnInit(){
+    this.data.getJokes().subscribe(result => {
+      this.joke = result;
+    });
+  }
 }
